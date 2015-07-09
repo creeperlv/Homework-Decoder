00adcc1c02c1f378848ace417cc9896136df1fe8
creeperlv <creeperlv@users.noreply.github.com>
2015-07-09 20:04:09 +0800

    Create Objects.java

---
+++ src/com/Studio2048/HomeworkDecoder/Objects.java
@@ -0,0 +1,7 @@
+package com.Studio2048.HomeworkDecoder;
+
+public class Objects {
+	public static int[] builds = { 13,2 };
+	public static String[] Versions = { "0.1.3","0.0.2" };
+	public static String[] status = { "Beta Preview","Beta Preview" };
+}
