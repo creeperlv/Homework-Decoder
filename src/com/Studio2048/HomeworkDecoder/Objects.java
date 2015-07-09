d451acbcd64f39ecdb41e6b531a951ef4cbcf183
creeperlv <creeperlv@users.noreply.github.com>
2015-07-09 20:05:47 +0800

    Update Objects.java

--- src/com/Studio2048/HomeworkDecoder/Objects.java
+++ src/com/Studio2048/HomeworkDecoder/Objects.java
@@ -4,4 +4,5 @@ public class Objects {
 	public static int[] builds = { 13,2 };
 	public static String[] Versions = { "0.1.3","0.0.2" };
 	public static String[] status = { "Beta Preview","Beta Preview" };
+	public static boolean isUsingPro=false;
 }
