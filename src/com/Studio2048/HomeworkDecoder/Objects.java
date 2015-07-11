d7350e5734b4cb83d04ade3e431b5a77d6a832fb
creeperlv <creeperlv@users.noreply.github.com>
2015-07-11 21:09:58 +0800

    Update Objects.java

--- src/com/Studio2048/HomeworkDecoder/Objects.java
+++ src/com/Studio2048/HomeworkDecoder/Objects.java
@@ -1,9 +1,13 @@
 package com.Studio2048.HomeworkDecoder;
 
 public class Objects {
-	public static int[] builds = { 14,3 };
-	public static String[] Versions = { "0.1.4","0.0.3" };
-	public static String[] status = { "Beta Preview","Beta Preview" };
-	public static boolean isUsingPro=false;
+	public static int[] builds = { 15, 6 };
+	public static String Key="0AP9-88U";
+	public static String[] Versions = { "0.1.5", "0.0.6" };
+	public static String[] status = { "Beta Preview", "Beta Preview" };
+	public static String[] Part = { "HmwkMain", "Hmwk_Debug" };
+	public static boolean isException=false;
+	public static boolean isUsingPro = false;
+	public static String Theme = "AeroLight";
+	public static Exception e=new Exception("NULL");
 }
-
