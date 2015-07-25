83b3940742deccbab2f0c328e808ae181c355a06
creeperlv <creeperlv@users.noreply.github.com>
2015-07-25 12:35:14 +0800

    Create Detail.js

---
+++ Detail.js
@@ -0,0 +1,1 @@
+com.Studio2048.Tip.Tip.explode("<html><body><p><font size=4 color=\"white\">This is a free trial version, you can use it for free,but we do not take any responsibility if it breaks your computer. The responsibilities includes provide patchs, help for fixing computer and pay for fixing computer.","Activate","./ActivatePro.js");
